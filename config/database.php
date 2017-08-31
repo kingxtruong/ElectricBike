3a26e7d5dc74cb3a004594bfe8145b510d07117a
kingxtruong <31484421+kingxtruong@users.noreply.github.com>
2017-08-31 22:55:36 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -55,6 +55,7 @@ return [
         ],
 
         'pgsql' => [
+            'default' => 'pgsql',
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
